a19af4fb08577a0892ea39432052647040c0d247
Gunny576 <garrettorme@gmail.com>
2016-09-08 15:37:55 -0500

    started work on Trigger code, updated querry interface code added minimal comments to code to clean it up

--- Flight/Flight/Controllers/HomeController.cs
+++ Flight/Flight/Controllers/HomeController.cs
@@ -9,58 +9,61 @@ using MySql.Data.MySqlClient;
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
-        //Test for Visual studio
     {
-        public string connStr = "server=24.124.62.140;user=Cis264;database=test;port=3306;password=P@ssword;";
+        //sql server login information
+        public string connStr = "server=localhost;user=cis264;database=test;port=3306;password=P@ssword;";
         
         public ActionResult Index()
         {
             MySqlConnection conn = new MySqlConnection(connStr);
             try
             {
-                Console.WriteLine("Connecting to MySQL...");
                 conn.Open();
-
-                string sql = "SELECT * FROM transaction limit 10";
+                //sql command for warning flag data
+                string sql = "SELECT * FROM `warning_flags` where handled = '0'";
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
                 MySqlDataReader rdr = cmd.ExecuteReader();
-                List<string> ListofColors = new List<string>();
+                //data we take from the database is stored here
                 List<string> data = new List<string>();
-                ListofColors.Add("red");
-                ListofColors.Add("green");
-                ListofColors.Add("blue");
-
-                ViewBag.ListColors = ListofColors;
-               
+                //if no data is added into the list and we try to pull from it, we get a crash
+                //this string exists mostly to prevent that
+                data.Add("These are the warning flags raised while you were away:");
+                //read loop, works like a file read loop, can go up to about 1million before my computer gives up
+                //and visual studio crashes
                 while (rdr.Read())
                 {
-                    data.Add(rdr[0] + " -- " + rdr[1] + " -- " + rdr[3] + " -- " + rdr[4]);
+                    data.Add(rdr[0] + " -- " + rdr[1] + " -- " + rdr[2] + " -- " + rdr[3]);
                     Console.WriteLine(rdr[0] + " -- " + rdr[1]);
                 }
+                //pass that data to the view for output
                 ViewBag.data = data;
-
+                //close the datastream from the server
                 rdr.Close();
             }
+            //exception handleing 
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
-
+            //close connection to the server
             conn.Close();
+            //call view to display output
             return View();
         }
 
 
 
+
+        //call for the querry interface
         [HttpGet]
-        public ActionResult TestLink()
+        public ActionResult querry()
         {
             return View();
         }
 
-
+        //handleing the querry interface createing a querry and returning
         [HttpPost]
-        public ActionResult TestLink(Models.TestData userData1)
+        public ActionResult querry(Models.TestData userData1)
         {
             MySqlConnection conn2 = new MySqlConnection(connStr);
             try
@@ -71,25 +74,19 @@ namespace WebApplication1.Controllers
                 string sql = "SELECT * FROM transaction where `transaction`.`transactionDate` between str_to_date('" + userData1.Name+ "','%m/%d/%Y') and str_to_date('" + userData1.Email+ "','%m/%d/%Y')";
                 MySqlCommand cmd = new MySqlCommand(sql, conn2);
                 MySqlDataReader rdr = cmd.ExecuteReader();
-                List<string> ListofColors = new List<string>();
                 List<string> date_data = new List<string>();
-                date_data.Add("Null Prevention test");
-                ViewBag.ListColors = ListofColors;
+                date_data.Add("The results are as follows:");
                 while (rdr.Read())
                 {
-                    ViewBag.datesingle=(rdr[0] + " -- " + rdr[1] + " -- " + rdr[3] + " -- " + rdr[4]);
                     date_data.Add(rdr[0] + " -- " + rdr[1] + " -- " + rdr[3] + " -- " + rdr[4]);
-                    Console.WriteLine(rdr[0] + " -- " + rdr[1]);
                 }
                 ViewBag.date_data = date_data;
-
                 rdr.Close();
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
-
             conn2.Close();
             return View("Thanks", userData1);
         }
